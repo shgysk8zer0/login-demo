b7446908c16198c3c99da69230f56a976f4e0ce7
Chris Zuber <shgysk8zer0@gmail.com>
2018-08-29 17:07:55 -0700

    Do not use destination URL

--- admin-redirect/index.php
+++ admin-redirect/index.php
@@ -1,17 +1,17 @@
 <?php
 namespace AdminRedirect;
 use function \Functions\{verify, array_keys_exist, login, redirect};
+use const \Constants\{DESTINATION};
 use \DateTime;
 require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . 'functions.php';
 
-if (array_keys_exist($_GET, 'user', 'url', 'datetime', 'hmac')) {
+if (array_keys_exist($_GET, 'user', 'datetime', 'hmac')) {
   if (verify(
     $_GET['user'],
-    $_GET['url'],
     new DateTime($_GET['datetime']),
     $_GET['hmac']
   ) and login($_GET['user'])) {
-    redirect($_GET['url']);
+    redirect(DESTINATION);
   } else {
     http_response_code(404);
   }
