2d9be4899794a0ada7f492bcc1f260bc99496b3d
Chris Zuber <shgysk8zer0@gmail.com>
2018-08-30 09:29:22 -0700

    Add own login accept/reject pages
    Also redirects to error page on login reject and errors.

--- admin-redirect/index.php
+++ admin-redirect/index.php
@@ -1,20 +1,27 @@
 <?php
 namespace AdminRedirect;
 use function \Functions\{verify, array_keys_exist, login, redirect};
-use const \Constants\{DESTINATION};
+use const \Constants\{SUCCESS_PAGE, ERROR_PAGE};
 use \DateTime;
+use \Throwable;
+use \Error;
+
 require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . 'functions.php';
 
-if (array_keys_exist($_GET, 'user', 'datetime', 'hmac')) {
-  if (verify(
-    $_GET['user'],
-    new DateTime($_GET['datetime']),
-    $_GET['hmac']
-  ) and login($_GET['user'])) {
-    redirect(DESTINATION);
+try {
+  if (array_keys_exist($_GET, 'user', 'datetime', 'hmac')) {
+    if (verify(
+      $_GET['user'],
+      new DateTime($_GET['datetime']),
+      $_GET['hmac']
+      ) and login($_GET['user'])) {
+        redirect(SUCCESS_PAGE);
+      } else {
+        throw new \Error('Invalid login attempt');
+      }
   } else {
-    http_response_code(400);
+    throw new \Error('Missing required paramaters');
   }
-} else {
-  http_response_code(400);
+} catch (Throwable $e) {
+  redirect(ERROR_PAGE);
 }
--- consts.php
+++ consts.php
@@ -1,7 +1,8 @@
 <?php
 namespace Constants;
-const KEY         = '3NP}]PK&=->`D-F}2v*krwHUy`d+#t9J$"J7p>bX';
-const ALGO        = 'sha3-512';
-const LIFETIME    = '+5 seconds';
-const USER        = 'mySpiffyUsername';
-const DESTINATION = 'https://shgysk8zer0.github.io/';
+const KEY          = '3NP}]PK&=->`D-F}2v*krwHUy`d+#t9J$"J7p>bX';
+const ALGO         = 'sha3-512';
+const LIFETIME     = '+5 seconds';
+const USER         = 'mySpiffyUsername';
+const SUCCESS_PAGE = '/success/';
+const ERROR_PAGE   = '/error/';
