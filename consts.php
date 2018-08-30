a72ecefc67f536b24fd4008807ca4795b1866626
Chris Zuber <shgysk8zer0@gmail.com>
2018-08-29 22:18:01 -0700

    Switch to SHA3-512

--- consts.php
+++ consts.php
@@ -1,7 +1,7 @@
 <?php
 namespace Constants;
 const KEY         = '3NP}]PK&=->`D-F}2v*krwHUy`d+#t9J$"J7p>bX';
-const ALGO        = 'sha512';
+const ALGO        = 'sha3-512';
 const LIFETIME    = '+5 seconds';
 const USER        = 'mySpiffyUsername';
 const DESTINATION = 'https://shgysk8zer0.github.io/';
