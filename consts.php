66e208155756c58995b4f2c4f9b64694a12c6851
Chris Zuber <shgysk8zer0@gmail.com>
2018-08-31 09:42:18 -0700

    Fix WP_INIT location

--- consts.php
+++ consts.php
@@ -6,5 +6,5 @@ const LIFETIME     = '+5 seconds';
 const USER         = 'mySpiffyUsername';
 const SUCCESS_PAGE = '/success/';
 const ERROR_PAGE   = '/error/';
-const WP_INIT      = __DIR__ . '/wp-load.php';
+define(__NAMESPACE__ . '\\WP_INIT', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'wp-load.php');
 define(__NAMESPACE__ . '\\IS_WP', @file_exists(WP_INIT));
